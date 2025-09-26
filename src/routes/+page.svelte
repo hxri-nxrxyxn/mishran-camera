4f771ad2dd4b23cd0a87c0628d42356cf87fc42a
RiziaPrabin <riziaprabin1910@gmail.com>
2025-09-26 22:07:56 +0530

    feat: Initialized necessary variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount, onDestroy } from "svelte"; //onDestroy is useed to release the camera and close the websocket when the component is closed
+    let camera = null; //camera will hold the media stream
+    let socket = null; //socket will hold the websocket connection to your server
+    let recorder = null; //recorder will hold the MediaRecorder used to produce small video/audio blobs
+    let videoelement;
+    let ipAddress = "";
+    let status = "Ready to connect";
+    let isConnecting = false;
+    //ipAddress, status and isConnecting are UI state variables
+</script>
